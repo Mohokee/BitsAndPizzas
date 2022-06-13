e096ef56ac3064db67b10b369c1a26d0e0fcaa98
Mohokee <j.inger.ly.co@gmail.com>
2022-06-13 12:17:07 -0700

    Added radio buttons, chips, a toast, and a SnackBar

--- app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
@@ -13,47 +13,58 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.chip.Chip
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.hfad.bitsandpizzas.databinding.FragmentOrderBinding
 
 
 class OrderFragment : Fragment() {
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_order, container, false)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        _binding = FragmentOrderBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        //VIEW BINDING-UNNEEDED val view = inflater.inflate(R.layout.fragment_order, container, false)
+        //VIEW BINDING-UNNEEDED val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         // cast the activity to AppCompatActivity to set the toolbar on a fragment
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
         //FAB
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        //VIEW BINDING-UNNEEDED val fab = view.findViewById<FloatingActionButton>(R.id.fab)
         //How to find selected radio button
-        fab.setOnClickListener{
+        binding.fab.setOnClickListener{
             //get radio button group
-            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            //VIEW BINDING-UNNEEDED val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
             //see which radio is checked
-            val pizzaType = pizzaGroup.checkedRadioButtonId
+            val pizzaType = binding.pizzaGroup.checkedRadioButtonId
             //if all are unchecked, the value is -1
             if(pizzaType == -1){
                 //toasts call .make text, then (this/activity,text,and length), then .show to display
                 Toast.makeText(activity,"Please Select a Pizza Type",Toast.LENGTH_LONG).show()
             }else{
-                val parm = view.findViewById<Chip>(R.id.parm)
-                val chili = view.findViewById<Chip>(R.id.chili_oil)
+                //VIEW BINDING-UNNEEDED val parm = view.findViewById<Chip>(R.id.parm)
+                //VIEW BINDING-UNNEEDED val chili = view.findViewById<Chip>(R.id.chili_oil)
                 var text = when(pizzaType){
                     R.id.pizza_diavolo -> "Diavolo pizza"
                     else -> "Funghi pizza"
                 }
-                text += if(parm.isChecked) ", extra parmesan" else " "
-                text += if(chili.isChecked) ", extra chili oil" else " "
+                text += if(binding.parm.isChecked) ", extra parmesan" else " "
+                text += if(binding.chiliOil.isChecked) ", extra chili oil" else " "
 
-                Snackbar.make(fab,"You are ordering a $text",Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.fab,"You are ordering a $text",Snackbar.LENGTH_LONG).show()
             }
 
         }
         // Inflate the layout for this fragment
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
